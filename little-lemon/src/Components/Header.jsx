0083f34007a1fe969e8542010a3e82c89fb088da
Melissa <141769451+melissx@users.noreply.github.com>
2024-01-20 16:43:53 +0000

    header done

--- little-lemon/src/Components/Header.jsx
+++ little-lemon/src/Components/Header.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import bannerImg from "./Images/restaurantfood.jpg";
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='header'>
+      <div className="headerContainer">
+        <div className="banner">
+          <h2>Little Lemon</h2>
+          <h3>Chicago</h3>
+          <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with an modern twist</p>
+        </div>
+        <button aria-label='On Click'><a href="Booking">Reserve a Table</a></button>
+      </div>
+      <div className="bannerImg">
+        <img src={bannerImg} alt="banner image" />
+      </div>
+    </header>
   )
 }
 
-export default Header
+export default Header;
